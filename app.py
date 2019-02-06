53be99d35c1cf1ceb3cfc2bb06982e2459a90e58
tomflynn1999 <tom@medshift.com>
2019-02-06 13:50:40 -0500

    fixed indentation

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ people = ["Tom", "Alex", "Carter", "Spencer", "Kyle","Maliek"]
 @app.route('/')
 def chores1():
     while (len(chores) < 0):
-          chore2 = random.choice(chores)
-          chores.remove(chore2)
-          person = random.choice(people)
-          people.remove(person)
+        chore2 = random.choice(chores)
+        chores.remove(chore2)
+        person = random.choice(people)
+        people.remove(person)
     return person + " needs to " + chore2
 
 
